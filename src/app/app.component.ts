5160d263284cdb8df69c78bb8561c2c4a591ec6d
Cristian Flores <cristianflores.ee@gmail.com>
2020-06-30 13:14:22 -0400

    fix: redirect uri

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import {
+  OidcClientNotification,
+  OidcSecurityService,
+  PublicConfiguration,
+} from "angular-auth-oidc-client";
+import { Observable } from "rxjs";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
-export class AppComponent {
-  title = 'ng-auth-implicit-flow';
+export class AppComponent implements OnInit {
+  title = "ng-auth-implicit-flow";
+
+  constructor(public oidcSecurityService: OidcSecurityService) {}
+
+  ngOnInit() {
+    this.oidcSecurityService.checkAuth().subscribe((auth) => {
+      console.log("is authenticated: ", auth);
+      const token = this.oidcSecurityService.getToken();
+      console.log("token: ", token);
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,59 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { APP_INITIALIZER, NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+
+import {
+  AuthModule,
+  LogLevel,
+  OidcConfigService,
+} from "angular-auth-oidc-client";
+import { HomeComponent } from "./home/home.component";
+import { LoginComponent } from "./login/login.component";
+export function configureAuth(oidcConfigService: OidcConfigService) {
+  return () =>
+    oidcConfigService.withConfig(
+      {
+        stsServer: "https://accounts.google.com",
+        redirectUrl: `${window.location.origin}/home`,
+        postLogoutRedirectUri: `${window.location.origin}/login`,
+        postLoginRoute: "/home",
+        forbiddenRoute: "/forbidden", // no hay permiso para acceder a recursos
+        unauthorizedRoute: "/unauthorized",
+        autoUserinfo: true,
+        clientId:
+          "599322351517-f0cb34grfekqot41daad1oq5fp1r7rov.apps.googleusercontent.com",
+        scope: "openid profile email",
+        responseType: "id_token token",
+        silentRenew: true,
+        silentRenewUrl: `${window.location.origin}/silent-renew.html`,
+        renewTimeBeforeTokenExpiresInSeconds: 30,
+        logLevel: LogLevel.Debug,
+      }
+
+      /*
+    triggerAuthorizationResultEvent: true,
+    startCheckSession: false,
+    
+    
+    
+    historyCleanupOff: true,*/
+    );
+}
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [AppComponent, HomeComponent, LoginComponent],
+  imports: [BrowserModule, AppRoutingModule, AuthModule.forRoot()],
+  providers: [
+    OidcConfigService,
+    {
+      provide: APP_INITIALIZER,
+      useFactory: configureAuth,
+      deps: [OidcConfigService],
+      multi: true,
+    },
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
